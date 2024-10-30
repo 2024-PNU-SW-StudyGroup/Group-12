54aaf158e323fcfaa29b318f507719f2079310d9
jungse8609 <jungse8609@naver.com>
2024-10-30 17:07:39 +0900

    [FEATURE] Player 아트 및 StateMachine 틀 스크립트 추가 - Texture가 날아가는 버그 존재 - StateMachine 틀만 추가하였고, Player 관련 스크립트는 추가해야 함

---
+++ Maggi/Assets/Scripts/StateMachine/Core/StateAction.cs
@@ -0,0 +1,21 @@
+﻿using Pudding.StateMachine.ScriptableObjects;
+
+namespace Pudding.StateMachine
+{
+	public abstract class StateAction : IStateComponent
+	{
+		internal StateActionSO _originSO;
+		protected StateActionSO OriginSO => _originSO;
+
+		public abstract void OnUpdate();
+		public virtual void OnFixedUpdate() { }
+		public virtual void Awake(StateMachine stateMachine) { }
+		public virtual void OnStateEnter() { }
+		public virtual void OnStateExit() { }
+
+		public enum SpecificMoment
+		{
+			OnStateEnter, OnStateExit, OnUpdate,
+		}
+	}
+}
